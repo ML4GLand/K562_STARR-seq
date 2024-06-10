8b185e7ff72570a72a5c4d6364ee25d315024886
adamklie <aklie@ucsd.edu>
2024-06-10 07:55:53 -0700

    v0.0.1-alpha

--- bin/2_create_bws.sh
+++ bin/2_create_bws.sh
@@ -7,10 +7,10 @@ samtools sort -@4 $path_data/merged_unsorted.bam -o $path_data/merged.bam
 samtools index $path_data/merged.bam
 
 # get coverage as bedgraph
-bedtools genomecov -bg -ibam $path_data/merged.bam -g $path_data/hg38.chrom.sizes > merged.bedgraph
+bedtools genomecov -bg -ibam $path_data/merged.bam -g $path_data/hg38.chrom.sizes | sort -k1,1 -k2,2n > $path_data/merged.bedgraph
 
 # convert bedgraph to bigwig
-bedGraphToBigWig $path_data/merged.bedgraph $path_data/hg38.chrom.sizes K562_STARR-seq_merged.bw
+bedGraphToBigWig $path_data/merged.bedgraph $path_data/hg38.chrom.sizes K562_STARR-seq_unstranded_counts.bw
 
 # remove intermediate files
 rm $path_data/merged_unsorted.bam $path_data/merged.bam $path_data/merged.bedgraph
